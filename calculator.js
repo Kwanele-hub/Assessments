efed76d9afca2c934c9725e23c090e76059381ca
nolwazi749 <nolwazizwane7@gmail.com>
2020-07-14 07:50:37 -0400

    i made changes

--- calculator.js
+++ calculator.js
@@ -1,75 +1,51 @@
-function addChar(input, character) {
-	if(input.value == null || input.value == "0")
-		input.value = character
-	else
-		input.value += character
+var clickedButton, equationDone = false;
+window.onload = function(){    
+    $('button').click(function(){        
+        clickedButton = $(this).attr('id');  
+        click();
+    });    
 }
 
-function cos(form) {
-	form.display.value = Math.cos(form.display.value);
+function click(){
+    if(clickedButton === 'clear')
+        document.getElementById('inputField').innerHTML = '0';
+    else if (clickedButton === 'divi' || clickedButton === 'multi' || clickedButton === 'sub' || clickedButton === 'dot' || clickedButton === 'add'){ 
+        if(document.getElementById('inputField').innerHTML === 'Error')
+            clear();
+        document.getElementById('inputField').innerHTML += document.getElementById(clickedButton).innerHTML;
+        equationDone = false;
+    }    
+    else if (clickedButton === 'equal'){        
+        var equation = document.getElementById('inputField').innerHTML;        
+        var answer;
+        
+        try {
+            eval(equation); 
+            answer = eval(equation);
+        } catch (e) {
+            if (e instanceof SyntaxError) {
+                document.getElementById('inputField').innerHTML = 'Error';
+            }
+        }       
+        
+        if(answer == 'Infinity')
+            document.getElementById('inputField').innerHTML = 'Error';  
+        else if(Number.isInteger(answer))//if it's an int don't round to two
+            document.getElementById('inputField').innerHTML = answer;               
+        else
+            document.getElementById('inputField').innerHTML = answer.toFixed(2);   
+        
+        equationDone = true;
+    }
+    else{       
+        clear();
+        document.getElementById('inputField').innerHTML += document.getElementById(clickedButton).innerHTML;        
+    }        
 }
 
-function sin(form) {
-	form.display.value = Math.sin(form.display.value);
-}
-
-function tan(form) {
-	form.display.value = Math.tan(form.display.value);
-}
-
-function sqrt(form) {
-	form.display.value = Math.sqrt(form.display.value);
-}
-
-function ln(form) {
-	form.display.value = Math.log(form.display.value);
-}
-
-function exp(form) {
-	form.display.value = Math.exp(form.display.value);
-}
-
-function deleteChar(input) {
-	input.value = input.value.substring(0, input.value.length - 1)
-}
-var val = 0.0;
-function percent(input) {
-  val = input.value;
-  input.value = input.value + "%";
-}
-
-function changeSign(input) {
-	if(input.value.substring(0, 1) == "-")
-		input.value = input.value.substring(1, input.value.length)
-	else
-		input.value = "-" + input.value
-}
-
-function compute(form) {
-  //if (val !== 0.0) {
-   // var percent = form.display.value;  
-   // percent = pcent.substring(percent.indexOf("%")+1);
-   // form.display.value = parseFloat(percent)/100 * val;
-    //val = 0.0;
- // } else 
-    form.display.value = eval(form.display.value);
-  }
-
-
-function square(form) {
-	form.display.value = eval(form.display.value) * eval(form.display.value)
-}
-
-function checkNum(str) {
-	for (var i = 0; i < str.length; i++) {
-		var ch = str.charAt(i);
-		if (ch < "0" || ch > "9") {
-			if (ch != "/" && ch != "*" && ch != "+" && ch != "-" && ch != "."
-				&& ch != "(" && ch!= ")" && ch != "%") {
-				alert("invalid entry!")
-				return false
-				}
-			}
-		}
-		return true
+function clear(){    
+    if(document.getElementById('inputField').innerHTML === '0' || equationDone === true || document.getElementById('inputField').innerHTML === 'Error')  
+        document.getElementById('inputField').innerHTML = '';
+    
+    equationDone = false;
 }
