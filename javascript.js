5ada5b7113c73d77af3ffbbfbf49c4bab16ae9d9
nolwazi749 <nolwazizwane7@gmail.com>
2020-10-14 02:51:33 -0400

    javascript code

--- javascript.js
+++ javascript.js
@@ -1,28 +1,44 @@
-var value = "aa"
-localStorage.setItem("testKey", value);
-var test = localStorage.getItem("testKey");
-alert(test);
-<form>
-  <label>Mortgage Amount</label>
-  <input type="text" id="amount">
-  <label>Interest Rate % </label>
-  <input type="text" id="interest">
-  <label>Mortgage Period (Years)</label>
-  <input type="text" id="period">
-  <input type="button" value="Submit" id="btn">
-  <input type="button" value="GetLocalStorage" id="localbtn">
-</form>
-         <script>
-$(document).ready(function () {
+$('document').ready(function()
+{
+            $("#login").on("submit", function(e) {              
+                e.preventDefault;
+                var btn = $('#btn-login');
+                btn.button('loading');
+                $.ajax({
+                    type: 'post',
+                    url: 'loginexec.php',
+                    cache: false,
+                    dataType: 'json',
+                    data: $('form#login').serialize(),
+                    beforeSend: function() { 
+                        $("#validation-errors").hide().empty(); 
+                    },
+                    success: function(data) {
+                        if(data.success == false)
+                            {
+                                var arr = data.errors;
+                                $.each(arr, function(index, value)
+                                {
+                                    if (value.length != 0)
+                                    {
+                                        $("#validation-errors").append('<div class="alert alert-danger"><strong>'+ value +'</strong><div>');
+                                    }
+                                });
+                                $("#validation-errors").show(); 
+                                btn.button('reset');                            
+                            } else {
+                                localStorage.login="true";
+                                localStorage.email=email;
+                                localStorage.id=id;
 
-  $("#btn").click(function () {
-
-      var principal = $("#amount").val();
-      var interest = $("#interest").val();
-      var years = $("#period").val();
-      var item = [];
-     item.push(principal,interest,years);
-     localStorage.item += JSON.stringify({ "principalAmount": principal, "interestAmount": interest, "Period": years });
-
-
-  });
+                                 window.location.href = 'user_profile.php';
+                            }
+                    },
+                    error: function(xhr, textStatus, thrownError) {
+                        alert('Something went to wrong.Please Try again later...');
+                        btn.button('reset');
+                    }
+                });             
+                return false;
+            });
+        });
